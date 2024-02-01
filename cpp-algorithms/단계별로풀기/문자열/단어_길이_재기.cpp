35422599acc7819c5ef1052f61e322db1a4f6349
hyunjinkim-kr <85622777+hyunjinkim-kr@users.noreply.github.com>
2024-02-01 13:53:54 +0900

    Create 단어_길이_재기.cpp

---
+++ cpp-algorithms/단계별로풀기/문자열/단어_길이_재기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+int main() {
+
+    string str1;
+    int n;
+    
+    cin >> str1;
+    
+    n = str1.length();
+
+    cout << n;
+    return 0;
+}
