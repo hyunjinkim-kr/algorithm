b8426a60f4f7e7478bb1c758d822dfbe0e484eaa
hyunjinkim-kr <85622777+hyunjinkim-kr@users.noreply.github.com>
2024-02-01 15:14:39 +0900

    Update and rename 문자와문자열.cpp to 27866.cpp

--- cpp-algorithms/단계별로풀기/문자열/27866.cpp
+++ cpp-algorithms/단계별로풀기/문자열/27866.cpp
@@ -3,17 +3,19 @@
 #include <algorithm>
 #include <set>
 
+//문자와 문자열
 using namespace std;
 int main() {
+
+    //여기서는, m만큼의 고정배열을 입력받고, n만큼의 값을 추가로 입력받을 것이다..
+    // set<int> mySet;
 
     string str1;
     int n;
     
     cin >> str1;
     cin >> n;
-    
-    str1 = str1.substr(n - 1, 1);
 
-    cout << str1;
+    cout << str1[n-1];
     return 0;
 }
