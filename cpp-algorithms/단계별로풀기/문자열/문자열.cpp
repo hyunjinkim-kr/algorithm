7961c2498e0f387c1097dac548c5ad30db1d7a37
hyunjinkim-kr <85622777+hyunjinkim-kr@users.noreply.github.com>
2024-02-01 14:06:34 +0900

    Create 문자열.cpp

---
+++ cpp-algorithms/단계별로풀기/문자열/문자열.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+int main() {
+
+    int m;
+    string str;
+
+    cin >> m;
+    vector<string> A(m + 1);
+
+    for (int i = 1; i <= m; i++) {
+        cin >> str;
+        A[i] = str;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        str = A[i];
+        str = str.substr(0,1) + str.substr(str.length()-1, 1);
+        cout << str << endl;
+    }
+    return 0;
+}
