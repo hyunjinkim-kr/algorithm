33127d53e3f8738f73cd572272456bcaa5e4fc0c
hyunjinkim-kr <85622777+hyunjinkim-kr@users.noreply.github.com>
2024-02-08 04:13:23 +0900

    Update quickSort.cpp

--- dataStructure/quickSort.cpp
+++ dataStructure/quickSort.cpp
@@ -6,33 +6,35 @@
 
 using namespace std;
 
-void swap(vector<int>& array, int copyIdx1, int copyIdx2) {
-    // 복사를 위한 변수
-    int copy = array[copyIdx1];
-    array[copyIdx1] = array[copyIdx2];
-    array[copyIdx2] = copy;
+enum PivotPolicy {
+    Naive,
+    MedianOfThree,
+};
+
+void swap(vector<int>& arr, int l, int r) {
+    int tmp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = tmp;
 }
 
-int choosePivot(bool isMot, vector<int>& array, int start, int end) {
-    if (isMot) {
-        int middle = start + (end - start) / 2;
+int choosePivot(vector<int>& arr, int start, int end, PivotPolicy policy) {
+    switch (policy) {
+    case Naive:
+        return start;
+    case MedianOfThree:
+        int middle = start + (end - start) / 2; // TODO: 이거 ㅄ임
         return middle;
-    } else {
+    Default:
         return start;
     }
 }
 
-void doQuickSort(vector<int>& array, int start, int end) {
-    // 정렬 시작
-    // 1. 피벗을 선정한다.
-    bool isMoT = true;
-
-    if (start >= end) // 원소가 1개인 경우 그대로 두기
+void quickSort(vector<int>& arr, int start, int end) {
+    if (start >= end) // 원소가 1개인 경우 리턴
         return;
 
-    // 1. 피벗을 선정한다.
-    int pivot = choosePivot(isMoT, array, start, end);
-    int pivotVal = array[pivot];
+    int pivot = choosePivot(arr, start, end, Naive);
+    int pivotVal = arr[pivot];
 
     // 왼쪽 포인터
     int i = start;
@@ -40,89 +42,68 @@ void doQuickSort(vector<int>& array, int start, int end) {
     int j = end;
 
     // 피벗을 기준으로 양쪽으로 나누기
-    while (i <= j) {
+    while (i < j) {
         // 피벗보다 큰 값을 찾으면 멈추기
-        while (i <= end && array[i] <= array[pivot]) {
+        while (i <= end && arr[i] <= arr[pivot]) {
             i++;
         }
 
         // 피벗보다 작은 값을 찾으면 멈추기
-        while (j >= start && array[j] > array[pivot]) {
+        while (j >= start && arr[j] >= arr[pivot]) {
             j--;
         }
 
         // 교차되지 않은 상태라면 값을 교환
         if (i < j) {
-            swap(array, i, j);
+            swap(arr, i, j);
         }
     }
 
-    // 피벗 위치 변경
-    swap(array, pivot, j);
-
+    // high 포인터가 범위를 벗어났을때
+    if (j < start)
+        j = start;
+    
+    swap(arr, pivot, j);
+       
     // 피벗을 기준으로 나뉜 두 부분에 대해 재귀 호출
-    doQuickSort(array, start, j - 1);
-    doQuickSort(array, j + 1, end);
+    quickSort(arr, start, j - 1);
+    quickSort(arr, j + 1, end);
 }
 
-bool validateCondition(const vector<int>& arr) {
-    int n = arr.size();
+bool validate(const vector<int>& v) {
+    vector<int> target = v, expected = v;
+    quickSort(target, 0, target.size() - 1);
+    std::sort(expected.begin(), expected.end());
+    return (target == expected);
+}
 
-    for (int i = 1; i < n; i++) {
-        if (arr[i - 1] > arr[i]) {
-            // 조건을 만족하지 않는 경우가 하나라도 발견되면 false 반환
-            return false;
+void test() {
+    for (int t = 0; t < 100000; ++t) {
+        // 시드 값 설정
+        srand(time(0));
+
+        // 1에서 7 사이의 랜덤한 길이 생성
+        int length = rand() % 7 + 1;
+
+        // 랜덤한 배열 생성 및 초기화
+        vector<int> org(length);
+
+        // 배열에 랜덤한 숫자 채우기
+        for (int i = 0; i < length; ++i) {
+            org[i] = rand() % 10 + 1;  // 1에서 10 사이의 랜덤한 숫자로 채우기
+        }
+ 
+        if (!validate(org)) {
+            for (int i = 0; i < length; i++) {
+                cout << org[i] << " ";
+            }
+            cout << endl;
+            exit(134);
         }
     }
-
-    // 모든 i, j에 대해 조건을 만족하는 경우 true 반환
-    return true;
 }
 
 int main() {
-    // 시드 값 설정
-    srand(time(0));
-
-    // 1에서 100 사이의 랜덤한 길이 생성
-    int length = rand() % 100+ 1;
-
-    // 랜덤한 배열 생성 및 초기화
-    vector<int> randomArray(length);
-
-    // 배열에 랜덤한 숫자 채우기
-    for (int i = 0; i < length; ++i) {
-        randomArray[i] = rand() % 100 + 1;  // 1에서 100 사이의 랜덤한 숫자로 채우기
-    }
-
-    // 수행 이전의 결과 출력
-    for (int k = 0; k < length; k++) {
-        cout << randomArray[k] << " ";
-    }
-    cout << " 수행이전" << endl;
-
-    doQuickSort(randomArray, 0, randomArray.size() - 1);  // 배열의 시작 주소와 끝 인덱스 전달
-
-    // 수행 이후의 결과 출력
-    // 퀵 정렬을 사용하여 벡터 정렬
-    std::sort(randomArray.begin(), randomArray.end());
-    for (int num : randomArray) {
-        std::cout << num << " ";
-    }
-    cout << " 알고리즘 수행결과" << endl;
-
-    // 수행 이후의 결과 출력
-    for (int k = 0; k < randomArray.size(); k++) {
-        cout << randomArray[k] << " ";
-    }
-    cout << " 최종결과" << endl;
-
-    // 조건 검증
-    if (validateCondition(randomArray)) {
-        cout << "배열이 조건을 만족합니다." << endl;
-    }
-    else {
-        cout << "배열이 조건을 만족하지 않습니다." << endl;
-    }
-
+    test();
     return 0;
 }
