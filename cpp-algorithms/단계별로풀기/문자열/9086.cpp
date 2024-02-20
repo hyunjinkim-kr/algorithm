490c21b1790f05cc43ba653f943e7cfaeb142290
hyunjin Kim <dufqnfsk22@gmail.com>
2024-02-20 09:57:39 +0900

    git test
    깃 테스트

--- cpp-algorithms/단계별로풀기/문자열/9086.cpp
+++ cpp-algorithms/단계별로풀기/문자열/9086.cpp
@@ -4,7 +4,8 @@
 #include <set>
 
 using namespace std;
-int main() {
+int main()
+{
 
     int m;
     string str;
@@ -12,15 +13,17 @@ int main() {
     cin >> m;
     vector<string> A(m + 1);
 
-    for (int i = 1; i <= m; i++) {
+    for (int i = 1; i <= m; i++)
+    {
         cin >> str;
         A[i] = str;
     }
 
-    for (int i = 1; i <= m; i++) {
+    for (int i = 1; i <= m; i++)
+    {
         str = A[i];
-        //str = str.substr(0,1) + str.substr(str.length()-1, 1);
-        cout << str[0] << str[str.length()-1] << endl;
+        // str = str.substr(0,1) + str.substr(str.length()-1, 1);
+        cout << str[0] << str[str.length() - 1] << endl;
     }
     return 0;
 }
