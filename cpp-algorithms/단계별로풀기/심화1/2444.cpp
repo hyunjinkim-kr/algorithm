8443ea1774a78ab4572aec2602bf28e2561859c7
hyunjin Kim <dufqnfsk22@gmail.com>
2024-02-21 10:02:28 +0900

    2444 반영

---
+++ cpp-algorithms/단계별로풀기/심화1/2444.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cmath>
+
+// 별 찍기 - 7
+using namespace std;
+
+int main()
+{
+    // 숫자 입력
+    int n;
+    string star_row;
+
+    cin >> n;
+
+    // n*2 -1만큼 포문을 돌아야한다.
+    for (int i = 1; i <= n * 2 - 1; i++)
+    {
+        star_row = "";
+        // 각 한줄에 나타나는 별 찍기
+        for (int j = 1; j <= n * 2 - 1; j++)
+        {
+            if (n - i >= 0) // 증가
+            {
+                if (i > abs(n - j))
+                {
+                    star_row += "*";
+                }
+                else
+                {
+                    if (n - j >= 0)
+                    {
+                        star_row += " ";
+                    }
+                }
+            }
+            else // 감소
+            {
+                if (2 * n - i > abs(n - j))
+                {
+                    star_row += "*";
+                }
+                else
+                {
+                    if (n - j >= 0)
+                    {
+                        star_row += " ";
+                    }
+                }
+            }
+        }
+        cout << star_row << endl;
+    }
+
+    return 0;
+}
