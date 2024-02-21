235eacdebc7934eaa0b2be1b4b02e6c1b51689d4
hyunjin Kim <dufqnfsk22@gmail.com>
2024-02-21 12:03:14 +0900

    10988 반영

---
+++ cpp-algorithms/단계별로풀기/심화1/10988.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cmath>
+
+// 팰린드롬인지 확인하기
+using namespace std;
+
+int check_pl(string str)
+{
+    int is_pl = 1;
+
+    for (int i = 0; i < str.length() / 2; i++)
+    {
+        if (str[i] != str[(str.length() - 1) - i])
+        {
+            is_pl = 0;
+        }
+    }
+
+    return is_pl;
+}
+
+int main()
+{
+    // 숫자 입력
+    string str;
+    int pl_Flag;
+
+    cin >> str;
+
+    pl_Flag = check_pl(str);
+
+    cout << pl_Flag << endl;
+
+    return 0;
+}
