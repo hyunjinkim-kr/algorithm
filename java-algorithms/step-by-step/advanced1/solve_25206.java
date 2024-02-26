f1b8c5ac634e93f2ba6c0fe5bf0e3b7beb079fda
hyunjin <dufqnfsk22@gmail.com>
2024-02-26 19:03:45 +0900

    25206 수정2

--- java-algorithms/step-by-step/advanced1/solve_25206.java
+++ java-algorithms/step-by-step/advanced1/solve_25206.java
@@ -10,7 +10,7 @@ public class solve_25206 {
         //문제 : 너의 평점은
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int subjectNum =20;
+        int subjectNum = 20;
         double calcGradeSum = 0.0;
         double gradeSum = 0.0;
 
@@ -19,7 +19,7 @@ public class solve_25206 {
             // 공백을 기준으로 문자열 분할
             String[] subjectGrade = str.split(" ");
             double calcGrade = getGrade(subjectGrade);
-            if(calcGrade >= 0){
+            if (calcGrade >= 0) {
                 gradeSum += Double.parseDouble(subjectGrade[1]);
                 calcGradeSum += calcGrade;
             }
@@ -30,7 +30,7 @@ public class solve_25206 {
     }
 
     // 입력받은 줄의 학점 * 평점을 반환
-    public static double getGrade(String[] subjectGrade){
+    public static double getGrade(String[] subjectGrade) {
         String[][] gradeArray = {
                 {"A+", "4.5"},
                 {"A0", "4.0"},
@@ -43,16 +43,17 @@ public class solve_25206 {
                 {"F", "0.0"}
         };
 
-        if(subjectGrade[2].equals("P")){
+        if (subjectGrade[2].equals("P")) {
             return -1;
         }
 
-        for(int i = 0; i < gradeArray.length; i++){
-            if(subjectGrade[2].equals(gradeArray[i][0])){
+        for (int i = 0; i < gradeArray.length; i++) {
+            if (subjectGrade[2].equals(gradeArray[i][0])) {
                 // 학점 * 과목평점
                 return Double.parseDouble(subjectGrade[1]) * Double.parseDouble(gradeArray[i][1]);
             }
         }
-        return -1;
+        throw new UnsupportedOperationException("도달할 수 없는 코드");
+        //return -1;
     }
 }
