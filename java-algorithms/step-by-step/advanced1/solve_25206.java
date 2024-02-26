78eb4304c731442ff44c35e878b5dc98b76ae3f2
hyunjin <dufqnfsk22@gmail.com>
2024-02-26 17:21:59 +0900

    25206 수정1

--- java-algorithms/step-by-step/advanced1/solve_25206.java
+++ java-algorithms/step-by-step/advanced1/solve_25206.java
@@ -11,30 +11,26 @@ public class solve_25206 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int subjectNum =20;
-
-        double calcGrade = 0.0;
+        double calcGradeSum = 0.0;
         double gradeSum = 0.0;
-        double avg = 0.0;
-        String str;
 
         for (int i = 0; i < subjectNum; i++) {
-            str = br.readLine();
+            String str = br.readLine();
             // 공백을 기준으로 문자열 분할
             String[] subjectGrade = str.split(" ");
-            calcGrade = getGrade(subjectGrade);
+            double calcGrade = getGrade(subjectGrade);
             if(calcGrade >= 0){
                 gradeSum += Double.parseDouble(subjectGrade[1]);
-                avg += calcGrade;
+                calcGradeSum += calcGrade;
             }
         }
         // 평균 = (P를 제외한) 학점 * 과목평점의 전체 합 / 학점의 총합
-        avg = avg / gradeSum;
+        double avg = calcGradeSum / gradeSum;
         System.out.print(avg);
     }
 
     // 입력받은 줄의 학점 * 평점을 반환
     public static double getGrade(String[] subjectGrade){
-        double grade = -1.0;
         String[][] gradeArray = {
                 {"A+", "4.5"},
                 {"A0", "4.0"},
@@ -47,13 +43,14 @@ public class solve_25206 {
                 {"F", "0.0"}
         };
 
-        if(!subjectGrade[2].equals("P")){
-            for(int i = 0; i < gradeArray.length; i++){
-                if(subjectGrade[2].equals(gradeArray[i][0])){
-                    // 학점 * 과목평점
-                    grade =  Double.parseDouble(subjectGrade[1]) * Double.parseDouble(gradeArray[i][1]);
-                    return grade;
-                }
+        if(subjectGrade[2].equals("P")){
+            return -1;
+        }
+
+        for(int i = 0; i < gradeArray.length; i++){
+            if(subjectGrade[2].equals(gradeArray[i][0])){
+                // 학점 * 과목평점
+                return Double.parseDouble(subjectGrade[1]) * Double.parseDouble(gradeArray[i][1]);
             }
         }
         return -1;
