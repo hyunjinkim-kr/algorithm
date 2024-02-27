9a2ea9d6c480b97def4d5c032ab668212dfef151
hyunjin <dufqnfsk22@gmail.com>
2024-02-28 00:13:05 +0900

    2566 반영4 (배열X)

--- java-algorithms/step-by-step/2D_Array/solve_2566.java
+++ java-algorithms/step-by-step/2D_Array/solve_2566.java
@@ -11,25 +11,23 @@ public class solve_2566 {
 
         // 배열을 잊어봅시다
         // String[][] doubleArray = new String[9][9];
-        
+
         //입력과 동시에 값을 출력하자.
         findMaxNum();
     }
 
     public static void findMaxNum() throws IOException {
         // 9줄 입력받아서 배열에 넣는 함수를 진행하자
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int maxNum = 0;
         int maxNumRow = 0;
         int maxNumCol = 0;
+        Scanner sc = new Scanner(System.in);
 
         for (int i = 0; i < 9; i++) {
-            String str = br.readLine();
-            // 공백을 기준으로 문자열 분할
-            ArrayList<String> arrayRow = new ArrayList<>(Arrays.asList(str.split(" ")));
-            for(int j = 0; j < arrayRow.size(); j++){
-                if(Integer.parseInt(arrayRow.get(j)) >= maxNum){
-                    maxNum = Integer.parseInt(arrayRow.get(j));
+            for(int j = 0; j < 9; j++){
+                int inputNum = sc.nextInt();
+                if(inputNum >= maxNum){
+                    maxNum = inputNum;
                     maxNumRow = i;
                     maxNumCol = j;
                 }
