b544633ed8fbe90d6b6c8489f6aecb422f2461bb
hyunjin <dufqnfsk22@gmail.com>
2024-02-27 23:45:01 +0900

    2566 반영3

--- java-algorithms/step-by-step/2D_Array/solve_2566.java
+++ java-algorithms/step-by-step/2D_Array/solve_2566.java
@@ -1,40 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class solve_2566 {
     public static void main(String[] args) throws IOException {
         //문제 : 최댓값
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[][] doubleArray = new String[9][9];
 
-        // 9 * 9 배열에 값넣기
-        inputArr(br,doubleArray);
-        findMaxNum(doubleArray);
+        // 배열을 잊어봅시다
+        // String[][] doubleArray = new String[9][9];
+        
+        //입력과 동시에 값을 출력하자.
+        findMaxNum();
     }
 
-    public static void inputArr(BufferedReader br, String[][] doubleArrayResult) throws IOException {
+    public static void findMaxNum() throws IOException {
         // 9줄 입력받아서 배열에 넣는 함수를 진행하자
-        for (int i = 0; i < doubleArrayResult.length; i++) {
-            String str = br.readLine();
-            // 공백을 기준으로 문자열 분할
-            doubleArrayResult[i] = str.split(" ");
-        }
-    }
-
-    public static void findMaxNum(String[][] doubleArrayResult) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int maxNum = 0;
         int maxNumRow = 0;
         int maxNumCol = 0;
 
-        // 9줄 입력받아서 배열에 넣는 함수를 진행하자
-        for (int i = 0; i < doubleArrayResult.length; i++) {
-            for (int j = 0; j < doubleArrayResult[i].length; j++) {
-                // 공백을 기준으로 문자열 분할
-                if(Integer.parseInt(doubleArrayResult[i][j]) >= maxNum){
-                    maxNum = Integer.parseInt(doubleArrayResult[i][j]);
+        for (int i = 0; i < 9; i++) {
+            String str = br.readLine();
+            // 공백을 기준으로 문자열 분할
+            ArrayList<String> arrayRow = new ArrayList<>(Arrays.asList(str.split(" ")));
+            for(int j = 0; j < arrayRow.size(); j++){
+                if(Integer.parseInt(arrayRow.get(j)) >= maxNum){
+                    maxNum = Integer.parseInt(arrayRow.get(j));
                     maxNumRow = i;
                     maxNumCol = j;
                 }
