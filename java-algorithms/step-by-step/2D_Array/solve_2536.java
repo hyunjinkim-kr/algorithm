a1523d154939857fbdf1390f3be7fa1a864e8044
hyunjin <dufqnfsk22@gmail.com>
2024-02-29 14:39:49 +0900

    2536 색종이 반영2

--- java-algorithms/step-by-step/2D_Array/solve_2536.java
+++ java-algorithms/step-by-step/2D_Array/solve_2536.java
@@ -10,33 +10,30 @@ public class solve_2536 {
 
         Scanner scan = new Scanner(System.in);
         Integer paperCount = scan.nextInt(); // 색종이 갯수
-
         ArrayList<ArrayList<String>> doubleArrayResult1 = new ArrayList<>();
 
-        for(int i = 0; i < paperCount; i++){
+        for (int i = 0; i < paperCount; i++) {
             Integer startX = scan.nextInt();
             Integer startY = scan.nextInt();
-
-            setPaper(i,rowNum,rowNum2,startX,startY,doubleArrayResult1 );
+            setPaper(i, rowNum, rowNum2, startX, startY, doubleArrayResult1);
         }
-        Integer ariaSize = calcAreaSize(rowNum,doubleArrayResult1 );;
+        Integer ariaSize = calcAreaSize(rowNum, doubleArrayResult1);
         System.out.print(ariaSize);
     }
 
-    public static void setPaper(Integer idxNum ,
-                                Integer rowNum ,
+    public static void setPaper(Integer idxNum,
+                                Integer rowNum,
                                 Integer rowNum2,
-                                Integer startX ,
-                                Integer startY ,
+                                Integer startX,
+                                Integer startY,
                                 ArrayList<ArrayList<String>> doubleArrayResult1) throws IOException {
 
-        if(idxNum == 0){
+        if (idxNum == 0) {
             for (int i = 0; i < rowNum; i++) {
                 ArrayList<String> newRow = new ArrayList<String>();
-
                 // 각 행에 배열 생성 및 입력값 저장
                 for (int j = 0; j < rowNum; j++) {
-                    if((i >= startX && i < startX + 10)&&(j >= startY && j < startY + 10)){
+                    if ((i >= startX && i < startX + 10) && (j >= startY && j < startY + 10)) {
                         newRow.add("O");
                     } else {
                         newRow.add("X");
@@ -46,10 +43,10 @@ public class solve_2536 {
             }
         } else {
             for (int i = 0; i < rowNum; i++) {
-                // 각 행에 배열 생성 및 입력값 저장
+                // 각 행에 입력값 저장
                 for (int j = 0; j < rowNum; j++) {
-                    if((i >= startX && i < startX + 10)&&(j >= startY && j < startY + 10)){
-                        doubleArrayResult1.get(i).set(j,"O");
+                    if ((i >= startX && i < startX + 10) && (j >= startY && j < startY + 10)) {
+                        doubleArrayResult1.get(i).set(j, "O");
                     }
                 }
             }
@@ -57,13 +54,13 @@ public class solve_2536 {
 
     }
 
-    public static Integer calcAreaSize(Integer rowNum,ArrayList<ArrayList<String>> doubleArrayResult1) throws IOException {
+    public static Integer calcAreaSize(Integer rowNum, ArrayList<ArrayList<String>> doubleArrayResult1) throws IOException {
 
         Integer countSize = 0;
         for (int i = 0; i < rowNum; i++) {
             // 각 행에 배열 생성 및 입력값 저장
             for (int j = 0; j < rowNum; j++) {
-                if(doubleArrayResult1.get(i).get(j).equals("O")){
+                if (doubleArrayResult1.get(i).get(j).equals("O")) {
                     countSize++;
                 }
             }
