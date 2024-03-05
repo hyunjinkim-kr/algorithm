47ba56503bbd63115a1bb1162eb1e8733ae276e7
hyunjin <dufqnfsk22@gmail.com>
2024-03-06 02:33:04 +0900

    2170 선긋기 반영3 : 성공

--- java-algorithms/step-by-step/solve_2170.java
+++ java-algorithms/step-by-step/solve_2170.java
@@ -1,73 +1,86 @@
 import java.io.*;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 //문제 : 선긋기
 public class solve_2170 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Integer rowCount = Integer.parseInt(br.readLine()); // 그어지는 선의 갯수
+
+        Integer rowSum = 0;
+
         // 배열을 까먹든지 색칠을 안하든지 해봐요
-        ArrayList<String> arrayResult1 = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> doubleArrayResult1 = new ArrayList<>();
 
         for (int i = 0; i < rowCount; i++) {
+
             String rowInfo = br.readLine();
-            arrayResult1.add(rowInfo);
+            StringTokenizer stRowInfo = new StringTokenizer(rowInfo," ");
+            Integer rowStart = Integer.valueOf(stRowInfo.nextToken());
+            Integer rowEnd = Integer.valueOf(stRowInfo.nextToken());
+
+            ArrayList<Integer> newRow = new ArrayList<Integer>();
+            newRow.add(rowStart);
+            newRow.add(rowEnd);
+            doubleArrayResult1.add(newRow);
         }
+        // 0번째 원소로 오름차순 정렬, 0번째가 같으면 1번째 원소로 내림차순 정렬
+        doubleArrayResult1.sort(Comparator.comparing((ArrayList<Integer> list) -> list.get(0), Comparator.naturalOrder())
+                .thenComparing((ArrayList<Integer> list1, ArrayList<Integer> list2) -> list2.get(1).compareTo(list1.get(1))));
 
-        String rowLength = calcRowLength(rowCount,arrayResult1).toString();
-        //System.out.print(rowLength);
-        bw.write(rowLength);
 
-        bw.flush();
-        bw.close();
+        Integer rowLength = calcRowLength(doubleArrayResult1);
+        System.out.print(rowLength);
     }
 
-    public static Integer calcRowLength(Integer rowCount, ArrayList<String> arrayResult1) throws IOException {
+    public static Integer calcRowLength(ArrayList<ArrayList<Integer>> doubleArrayResult1) throws IOException {
 
-        Integer rowLengthSum = 0;
-        Integer duplicatedLengthSum = 0;
+        // 최종적으로 결과를 넣을 녀석.
+        ArrayList<ArrayList<Integer>> doubleArrayResult2 = new ArrayList<>();
 
-        for (int i = 0; i < rowCount; i++) {
-            StringTokenizer rowInfoI = new StringTokenizer(arrayResult1.get(i)," ");
-            Integer startI = Integer.valueOf(rowInfoI.nextToken());
-            Integer endI = Integer.valueOf(rowInfoI.nextToken());
+        // 첫번째꺼는 무조건 넣어주기.
+        doubleArrayResult2.add(doubleArrayResult1.get(0));
+        Integer IdxJ = 0;
 
-            rowLengthSum += (endI - startI);
-            // 각 행에 배열 생성 및 입력값 저장
-            for (int j = i; j < rowCount; j++) {
-                if(j == i){
-                    continue;
-                }
-                StringTokenizer rowInfoJ = new StringTokenizer(arrayResult1.get(j)," ");
-                Integer startJ = Integer.valueOf(rowInfoJ.nextToken());
-                Integer endJ = Integer.valueOf(rowInfoJ.nextToken());
+        // input이 들어올때, 연결가능한 요소가 있는지 비교하고 없으면 넣자.
+        for(int i = 0; i < doubleArrayResult1.size(); i++){
 
-                // 겹치는 조건 만족.
-                // i줄 끝점이 j줄 시작점보다 크거나 같으면서 j줄 끝점보다 작거나 같을때
-                // i줄 시작점이 j점 시작점보다 크거나 같으면서 j줄 끝점보다 같거나 작을떄
-                if ((endI >= startJ && endI <= endJ)
-                    || (startI >= startJ && startI <= endJ)){
+            Integer startI = doubleArrayResult1.get(i).get(0);
+            Integer endI = doubleArrayResult1.get(i).get(1);
+            Integer startJ = doubleArrayResult2.get(IdxJ).get(0);
+            Integer endJ = doubleArrayResult2.get(IdxJ).get(1);
 
-                    Integer minStart = calcMinStart(startI , startJ);
-                    Integer maxEnd = calcMaxEnd(endI , endJ);
-
-                    duplicatedLengthSum += (endI - startI) + (endJ - startJ) - (maxEnd - minStart);
-                    // 1. 두 줄의 단순 길이 합을 구함
-                    // 2. 두 줄의 최소점과 끝점을 구해 합쳐진 길이를 구함
-                    // 3. 두개를 뺴면, 겹치는 길이가 나옴
-                    // 4. 겹치는 길이 제거값을 누적함.
-                }
+            if(startI.equals(startJ)){
+                continue;
             }
+
+            // 겹치는 조건 만족.
+            // i줄 끝점이 j줄 시작점보다 크거나 같으면서 j줄 끝점보다 작거나 같을때
+            // i줄 시작점이 j점 시작점보다 크거나 같으면서 j줄 끝점보다 같거나 작을떄
+            // endI 이 친구가 후발주자.
+            if (startI <= doubleArrayResult2.get(IdxJ).get(1)){
+
+                Integer minStart = calcMinStart(startI , startJ);
+                Integer maxEnd = calcMaxEnd(endI , endJ);
+                doubleArrayResult2.get(IdxJ).set(0,minStart);
+                doubleArrayResult2.get(IdxJ).set(1,maxEnd);
+            } else {
+                doubleArrayResult2.add(doubleArrayResult1.get(i));
+                IdxJ++;
+            }
+
         }
-        return rowLengthSum - duplicatedLengthSum;
+
+        Integer lengthSum = 0;
+        for(int k = 0; k < doubleArrayResult2.size(); k++){
+            lengthSum += doubleArrayResult2.get(k).get(1) - doubleArrayResult2.get(k).get(0);
+        }
+        return lengthSum;
     }
     public static Integer calcMinStart(Integer startI, Integer StartJ){
+        Integer minStart = 0;
         if(startI > StartJ){
             return StartJ;
         }
@@ -75,9 +88,12 @@ public class solve_2170 {
     }
 
     public static Integer calcMaxEnd(Integer endI, Integer endJ){
+        Integer minStart = 0;
         if(endI > endJ){
             return endI;
         }
         return endJ;
     }
 }
+
+
