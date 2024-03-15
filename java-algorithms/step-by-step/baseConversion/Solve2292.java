415ff5cb16ddeda6a6519110683a5d96954065ad
hyunjin <dufqnfsk22@gmail.com>
2024-03-15 14:24:15 +0900

    2292 벌집 코멘트 수정 :

--- java-algorithms/step-by-step/baseConversion/Solve2292.java
+++ java-algorithms/step-by-step/baseConversion/Solve2292.java
@@ -24,6 +24,27 @@ import java.util.stream.LongStream;
 // 고로, 3n^2 - 3n + 1 >= B 의 양의 최솟값을 구하는 근의 공식을 수행한다.
 //
 // --시간복잡도 : 1
+//
+// 해결책2. 이진탐색 풀이 (lower bound)
+// 위에서 계산된 계차수열 3n^2 - 3n + 1 에 대하여 이진탐색 수행
+//
+// begin : 시작값. 초기값은 1
+// mid : begin과 end를 2로 나눈 값
+// end : 끝값. 초기값은 100만
+//
+// 1. 시작점과 끝점을 설정한다. (B가 10억까지 입력 가능할떄 n의 최대값은 57만 즈음이므로, 100만으로 end설정)
+// 2. 탐색에 맞는 방법을 선택한다. -> lower bound. 최초로 일치하는 값 찾기.(사실 중복된 값이 없으므로 그냥 search해도 됨.)
+//  - 3n^2 - 3n + 1이 B보다 크거나 같은 최초의 n을 찾아야한다. -> 1 ~ 100만
+//  - 시작 1과 end 100만을 더해서 2로 나눈다. -> 50만.
+//  - mid를 n에 대입한 n^2 - 3n + 1 값이 B보다 작은경우, n이 너무 작으므로 그 뒤의 범위를 계산해야한다.
+//    따라서 begin을 mid + 1로 변경한다. -> (50만 + 1) ~ 100만
+//  - 반대로 같거나 작은 경우,이전범위~현재 mid를 탐색해야한다.
+//    따라서 end를 mid로 변경하고 이전 범위를 탐색한다. -> 1 ~ 50만
+//  - 위 행위를 반복하다가, begin이 end와 같아질때 종료 후 end를 리턴한다.
+//
+// 복잡도 : log 1000000 : (1/2)^k * n = 1, 2^k = n, k = log n
+//
+// -- 시간복잡도 = log n
 public class Solve2292 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,6 +54,7 @@ public class Solve2292 {
         System.out.print((int) roomLevel);
     }
 
+    //이진탐색 풀이 (lower bound) -- 시간복잡도 = log n
     public static double findMinimumN(double B) {
         // 이차방정식의 계수
         // 계차수열 일반항 : 3n^2 - 3n + 1 >= B 이며, n의 최소값.
@@ -56,7 +78,7 @@ public class Solve2292 {
     }
 }
 
-// 풀이 2. 계차수열 이차방정식 풀이
+// 풀이 2. 계차수열 이차방정식 풀이 --시간복잡도 : 1
 //public static double findMinimumN(double B) {
 //    // 이차방정식의 계수
 //    // 계차수열 일반항 : 3n^2 - 3n + 1 >= B 이며, n의 최소값.
