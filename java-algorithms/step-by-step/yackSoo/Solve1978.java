e9ea7c3da406aaaad333e5793126af35289fc775
hyunjin <dufqnfsk22@gmail.com>
2024-03-22 15:52:15 +0900

    1978 소수찾기 반영 : 소수 구하는 로직 변경(소수 리스트 생성, 제곱근 이하의 소수 체크 반복)

--- java-algorithms/step-by-step/yackSoo/Solve1978.java
+++ java-algorithms/step-by-step/yackSoo/Solve1978.java
@@ -10,44 +10,75 @@ import java.util.StringTokenizer;
 public class Solve1978 {
     public static void main(String[] args) throws IOException {
         // 문제 : 소수찾기
-        // 해결책 : 입력받은 숫자가 1이 아니고, 약수가 2개인 경우를 판별한다.
+        // 해결책 : 소수리스트를 구하고, 입력받은 숫자를 소수 리스트 이내에서 찾는다.
         //
-        // no1 : 입력받은 숫자
+        // M : 체크할 숫자의 개수
+        // N : 2 ~ 최대값의 제곱근 사이의 정수
+        // N" : 체크할 숫자
+        // arr1 : 소수가저장되는 리스트
         //
-        // 1. 입력받은 숫자가 1인지 판별한다.
-        // 2. 1이 아니면, 제곱근만큼 수행하여 제곱근 이하의 약수의 갯수를 확인한다.
-        // 3. 소수는 1과 자기 자신만 짝이되어야 하는데, 제곱근 이하의 약수가 1개를 넘으면 소수가 될 수 없다.
-        // -- 복잡도 : 루트no1
-        // -- 시간복잡도 : O(루트no1)
+        // 1. 2부터 최대값 1000의 제곱근 범위의 소수를 구한다.
+        //  -- 복잡도 : 루트 1000 - 2
+        // 2. 소수로 판별된 숫자를 리스트에 저장한다. 2부터 루트 1000 사이의 숫자 N을 반복체크하며 이전 리스트를 참고한다.
+        //  -- 복잡도 : 루트 N
+        // 3. M개만큼 입력받은 숫자 N"를 소수 리스트 범위내에서 탐색한다.
+        //  -- 입력받은 숫자의 제곱근 만큼만 탐색하면 된다. 즉, 소수리스트를 탐색하다가 소수가 제곱근보다 커지면 멈춘다.
+        //  -- 복잡도 : 루트 N"
+        //
+        // -- 시간복잡도 : O( (루트1000 - 2)*루트N + M * 루트N" )
         Scanner scan = new Scanner(System.in);
         Integer numCount = scan.nextInt(); // 체크할 숫자 개수
         Integer count = 0;
 
+        Integer maxInput = 1000;
+        ArrayList<Integer> primeList = new ArrayList<>();
+        primeList = getPrimeList(maxInput);
+
         for (int i = 0; i < numCount; i++) {
-            Integer no1 = scan.nextInt();
-            if (isDecimal(no1)) {
+            Integer N = scan.nextInt();
+            if (isPrime(N,primeList)) {
                 count++;
             }
         }
         System.out.print(count);
     }
 
-    public static boolean isDecimal(Integer no1) {
+    // 1000이하의 소수 리스트를 반환한다.
+    public static ArrayList<Integer> getPrimeList(Integer maxInput) {
+        ArrayList<Integer> primeList = new ArrayList<>();
+        double maxPrimeNum = Math.sqrt(maxInput);
+
+        // 33.xx 아래의 소수를 찾아서 리스트에 넣는게 목표..
+        for (int i = 2; i <= maxPrimeNum; i++) {
+            boolean isAddList = isPrime(i,primeList);
+            if (isAddList) {
+                primeList.add(i);
+            }
+        }
+        return primeList;
+    }
+
+    public static boolean isPrime(Integer N, ArrayList<Integer> primeList) {
+        // 올바르게 동작하기 위한 primeList의 조건.
+        // 1.primeList는 정렬되어야 한다.
+        // 2.무조건 소수여야한다.
+        // 3.제곱근 N 이하의 소수가 있어야한다.
+
         // 소수는 1을 제외한 자기 자신을 약수로 가지는놈.
-        if (no1 == 1) {
+        if (N == 1) {
             return false;
         }
 
-        double no1Sqrt = Math.sqrt(no1); //약수의 범위를 제곱근으로 제한한다
-        Integer count = 0;
+        double no1Sqrt = Math.sqrt(N); //약수의 범위를 제곱근으로 제한한다
 
-        for (int i = 1; i <= no1Sqrt; i++) {
-            //약수가 아니면 continue;
-            if (no1 % i != 0) {
-                continue;
+        for (int i = 0; i < primeList.size(); i++) {
+            Integer primeNum = primeList.get(i);
+
+            if (primeNum > no1Sqrt) {   // 범위를 벗어났음. 계산종료
+                break;
             }
-            count++;
-            if (count > 1) {
+
+            if (N % primeNum == 0) {
                 return false;
             }
         }
