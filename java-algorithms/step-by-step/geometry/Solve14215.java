d524ff181e3e4309e080232262961d39309afb3c
hyunjin <dufqnfsk22@gmail.com>
2024-03-26 15:35:51 +0900

    14215 수정 : 배열, 정렬x

--- java-algorithms/step-by-step/geometry/Solve14215.java
+++ java-algorithms/step-by-step/geometry/Solve14215.java
@@ -11,11 +11,11 @@ public class Solve14215 {
         //
         // 해결책 : 가장 긴 선이 나머지 두개 합보다 작게 만든후 합을 구한다.
         //
-        // 1. 입력받은 세개 선을 정렬한다.
+        // 1. 입력받은 세개 선의 최대값을 구한다
         // 2. 가장 긴 선이 나머지 선의 합보다 작으면 단순 합이 둘레이다.
         // 3. 아니라면, 나머지 선의 합보다 1 작은 숫자를 가장 긴 선으로 하여 계산한다.
         //
-        // 시간복잡도 : O(NlogN)
+        // 시간복잡도 : O(1)
 
         Scanner scan = new Scanner(System.in);
         Integer line1 = scan.nextInt();
@@ -27,24 +27,22 @@ public class Solve14215 {
         lineArr.add(line2);
         lineArr.add(line3);
 
-        // 오름차순 정렬
-        Collections.sort(lineArr);
-
-        Integer result = getTriangleRound(lineArr);
+        Integer result = getTriangleRound(line1,line2,line3);
         System.out.print(result);
     }
 
-    public static Integer getTriangleRound(ArrayList<Integer> lineArr) {
+    public static Integer getTriangleRound(Integer line1, Integer line2, Integer line3) {
 
-        Integer line1 = lineArr.get(0);
-        Integer line2 = lineArr.get(1);
-        Integer line3 = lineArr.get(2);
+        Integer maxLine = Math.max(line1,line2);
+        maxLine = Math.max(maxLine,line3);
 
-        if (line3 < line1 + line2) {
-            return line1 + line2 + line3;
+        Integer sumLine = line1 + line2 + line3;
+
+        if (maxLine < sumLine - maxLine) {
+            return sumLine;
         }
 
-        return 2 * (line1 + line2) - 1;
+        return 2 * (sumLine - maxLine) - 1;
     }
 }
 
