675b2e5390156486d8360274c7bb62773ef89cc0
hyunjin <dufqnfsk22@gmail.com>
2024-03-28 15:47:06 +0900

    2798 반영3 : 블랙잭

--- java-algorithms/step-by-step/bruteForce/Solve2798.java
+++ java-algorithms/step-by-step/bruteForce/Solve2798.java
@@ -3,6 +3,7 @@ package bruteForce;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Solve2798 {
     public static void main(String[] args) throws IOException {
@@ -12,8 +13,8 @@ public class Solve2798 {
         // 1. 입력받은 카드들을 리스트에 넣는다.
         // 2. 카드를 찾을때마다 리스트를 한번씩 탐색한다.
         // 3. 이전 카드 위치의 이후부터 다음카드를 뽑는다.
-        // 4. 만약 목표하는 숫자와 일치하는 합이 나올경우, 바로 탐색을 멈춘다.
-        // 5. 일치하는 합이 나오지 않는경우, 근사값을 업데이트 한 후 반환한다.
+        // 4. 3장의 카드를 뽑았을때, 카드의 합을 저장한다.
+        // 5. 카드의 합이 이전 저장된 값보다 클때, 값을 바꾼다.
         //
         // 탐색횟수로 치면, nC3 = n*(n-1)*(n-2)/ 3*2*1
         // (n^3 - 3n^2 + 2n) / 6
@@ -27,45 +28,54 @@ public class Solve2798 {
         Integer N = 3; // 뽑을 카드의 갯수
 
         ArrayList<Integer> arr = new ArrayList<>();
-        ArrayList<Integer> acc = new ArrayList<>();
 
         for (int i = 0; i < cardCount; i++) {
             Integer newCard = scan.nextInt();
             arr.add(newCard);
         }
 
-        Integer lowerBound = 0;
         Result max = new Result(0);
+        // closure
+        Function<ArrayList<Integer>, Void> setMax = comb -> {
+            Integer sum = 0;
+            for (Integer idx: comb) {
+                sum += arr.get(idx);
+            }
+            if (sum > max.value && sum <= M) {
+                max.value = sum;
+            }
+            return null;
+        };
 
-        comb(arr, N, lowerBound, acc, max, M);
+        combination(0, arr.size(), N, setMax);
         System.out.print(max.value);
     }
 
-    public static void comb(ArrayList<Integer> arr,
-                            Integer N,
-                            Integer lowerBound,
-                            ArrayList<Integer> acc,
-                            Result max,
-                            Integer M) {
-        if (N == 0) {
-            setMax(acc, max, M);
-            return;
-        }
-
-        for (int i = lowerBound; i <= arr.size() - N; i++) {
-            acc.add(arr.get(i));
-            comb(arr, N - 1, i + 1, acc, max, M);
-            acc.remove(acc.size() - 1);
-        }
+    public static void combination(Integer start,
+                              Integer end,
+                              Integer N,
+                              Function<ArrayList<Integer>, Void> f) {
+        combination_inner(start, end, N, f, new ArrayList<>());
     }
 
-    public static void setMax(ArrayList<Integer> acc, Result max, Integer M) {
-        Integer accSum = 0;
-        for (int j = 0; j < acc.size(); j++) {
-            accSum += acc.get(j);
+    public static void combination_inner(Integer start,
+                            Integer end,
+                            Integer N,
+                            Function<ArrayList<Integer>, Void> f,
+                            ArrayList<Integer> acc) {
+        if (N == 0) {
+            f.apply(acc);
+            return;
         }
-        if (accSum > max.value && accSum <= M) {
-            max.value = accSum;
+        
+        if (end - start < N) {
+            // TODO: 범위 부족 Error
+        }
+
+        for (int i = start; i <= end - N; i++) {
+            acc.add(i);
+            combination_inner(i + 1, end,N - 1, f, acc);
+            acc.remove(acc.size() - 1);
         }
     }
 }
