f38601b6576971269f571080000f67c9f919bc82
hyunjin <dufqnfsk22@gmail.com>
2024-04-06 14:33:16 +0900

    25305 반영 : 커트라인

---
+++ java-algorithms/step-by-step/sort/Solve25305.java
@@ -0,0 +1,38 @@
+package sort;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solve25305 {
+    public static void main(String[] args) throws IOException {
+        /*
+         문제 : 커트라인
+         수를 입력받고 정렬한 후 결과 출력
+
+         1. 수를 입력받은후 내림차순으로 정렬
+         2. 해당하는 커트라인에 해당하는 사람 점수를 출력
+
+         시간복잡도 : N + NlogN
+         O(NlogN)
+        */
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> numArr = new ArrayList<>();
+
+        Integer peopleCnt = scan.nextInt();
+        Integer cutLine = scan.nextInt();
+
+        IntStream.range(0, peopleCnt).forEach(s -> numArr.add(scan.nextInt()));
+        // 내림차순으로 정렬
+        Collections.sort(numArr, Comparator.reverseOrder());
+        showResult(numArr, cutLine);
+    }
+
+    public static void showResult(ArrayList<Integer> numArr, Integer cutLine) {
+        // 중간값
+        System.out.println(numArr.get(cutLine - 1));
+    }
+}
