3d8643cd66c38c04a9374e0aa563a91a41746961
hyunjin <dufqnfsk22@gmail.com>
2024-04-11 20:09:38 +0900

    10815 반영 : 숫자 카드

---
+++ java-algorithms/step-by-step/setAndMap/Solve10815.java
@@ -0,0 +1,61 @@
+package setAndMap;
+
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solve10815 {
+    public static void main(String[] args) throws IOException {
+        // 문제 : 숫자 카드
+        // 해결책 : 숫자를 hashSet에 넣고, 입력받는 숫자가 있는지 탐색한다.
+        //
+        // H : 숫자를 저장할 hashSet
+        // N : 저장된 숫자 개수
+        // R : 테스트 결과를 저장하는 리스트
+        // N' : 테스트 숫자 개수
+        //
+        // 1. 입력받은 숫자 N개를 저장한다.
+        // 복잡도 : N
+        // 2. 테스트 숫자가 H에 저장되어있는지 확인하고 결과를 R에 저장한다.
+        // 복잡도 : 2 * N'
+        // 3. 테스트 결과를 출력한다.
+        // 복잡도 : N'
+        //
+        // 시간복잡도 : O(N)
+        HashSet hm = new HashSet<>();
+        ArrayList<Integer> resultArr = new ArrayList<>();
+
+        try (InputStreamReader isr = new InputStreamReader(System.in)) {
+            try (BufferedReader br = new BufferedReader(isr)) {
+                // Logic 구현부
+                Integer numCnt = Integer.parseInt(br.readLine());
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                for (int i = 0; i < numCnt; i++) {
+                    Integer inputNum = Integer.parseInt(st.nextToken());
+                    hm.add(inputNum);
+                }
+
+                Integer findNumCnt = Integer.parseInt(br.readLine());
+                StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+                for(int j = 0; j < findNumCnt; j++){
+                    Integer testNum = Integer.parseInt(st2.nextToken());
+                    if(hm.contains(testNum)){
+                        resultArr.add(1);
+                        continue;
+                    }
+                    resultArr.add(0);
+                }
+            }
+        }
+
+        try (OutputStreamWriter osw = new OutputStreamWriter(System.out)) {
+            try (BufferedWriter bw = new BufferedWriter(osw)) {
+                final String resultStr = resultArr.stream().map(String::valueOf).collect(Collectors.joining(" "));
+                bw.write(resultStr);
+            }
+        }
+    }
+}
