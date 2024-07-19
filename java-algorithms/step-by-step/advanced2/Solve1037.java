f5c35cdb20c68bab4a1982ecdd23c8aee07c0f9c
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-07-20 00:57:02 +0900

    심화2 약수반영 Solve1037

--- java-algorithms/step-by-step/advanced2/Solve1037.java
+++ java-algorithms/step-by-step/advanced2/Solve1037.java
@@ -5,6 +5,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
+//문제 : 약수
+//해결책 : 입력받은 약수를 정렬한 후, 가장 작은수와 큰수를 곱한다.
+//
+// N : 구해야하는 수
+// A : 1과 N이 아닌 약수
+// T : N에 대한 A의 개수
+//
+// - 1. T를 입력받는다.
+//
+// - 2. A를 T만큼 입력받으며, 각각 배열에 저장한다.
+//        -- 복잡도 : T
+//
+// - 3. A가 저장된 T길이의 배열을 정렬한다.
+//        -- 복잡도 : TlogT
+//
+// - 4. 배열의 맨앞과 맨뒤의 값을 곱한다.
+//
+//시간복잡도 : NlogN
+
 public class Solve1037 {
     public static void main(String[] args) throws IOException {
         Integer result;
