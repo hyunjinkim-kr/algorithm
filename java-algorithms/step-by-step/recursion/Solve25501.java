98a896e9b5bae72d526e8ba4c59c4093d8ec43c1
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-07-24 15:18:19 +0900

    Solve25501 재귀의 귀재 반영

---
+++ java-algorithms/step-by-step/recursion/Solve25501.java
@@ -0,0 +1,49 @@
+package recursion;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Solve25501 {
+    // 문제 : 재귀의 귀재
+
+    // 해결책 : 재귀 함수에 진입횟수를 저장하는 파라미터와 결과값을 같이 리턴한다.
+    //
+    // t : 입력받은 숫자
+    // s : t번 만큼 입력받는 문자열
+    // cnt : 재귀가 수행된 횟수
+    // result : 팰린드롬 여부와 재귀횟수가 작성된 문자열
+    // resultArr : result가 저장된 배열
+    //
+    // 1. t을 입력받는다.
+    //
+    // 2. t만큼 s를 입력받고, 각각 재귀를 수행한다. 재귀 첫수행시 cnt는 1이고, 재귀가 수행됨에 따라 1씩 증가한다.
+    //      복잡도 t * cnt
+    //
+    // 3. 수행된 결과 result가 담긴 resultArr을 출력한다.
+    //
+    // -- 시간복잡도 O(N^2)
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> resultArr = new ArrayList<>();
+        try (InputStreamReader isr = new InputStreamReader(System.in);
+             BufferedReader br = new BufferedReader(isr)) {
+            Integer t = Integer.parseInt(br.readLine());
+
+            for(int i = 0; i < t; i++){
+                String s = br.readLine();
+                String result = isPalindrome(s);
+                resultArr.add(result);
+            }
+        }
+        try (OutputStreamWriter osw = new OutputStreamWriter(System.out); BufferedWriter bw = new BufferedWriter(osw)) {
+            bw.write(String.join("\n",resultArr));
+        }
+    }
+    public static String isPalindrome(String s){
+        return recursion(s, 0, s.length()-1,1);
+    }
+    public static String recursion(String s, int l, int r, int cnt){
+        if(l >= r) return 1 + " " + cnt;
+        else if(s.charAt(l) != s.charAt(r)) return 0 + " " + cnt;
+        else return recursion(s, l+1, r-1,cnt +1);
+    }
+}
