8b55c7045b02beca2ee23fa95dec6bdf06494903
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-07-29 01:07:30 +0900

    일단 수정중

--- java-algorithms/step-by-step/advanced2/Solve2447.java
+++ java-algorithms/step-by-step/advanced2/Solve2447.java
@@ -37,41 +37,63 @@ public class Solve2447 {
         // 2. 구멍을 뜷어야 하는 사이즈
         // 3. 구멍을 뜷어야 하는 위치
         Integer holeSize = N/3;
-        Integer start = N/3;
-        Integer end = 2 * N/3;
-        return starHelper(starArr, N, holeSize, holeSize,start,end);
+        Integer x = N/3;
+        Integer y = N/3;
+        return starHelper(starArr, N, holeSize, x ,y,holeSize);
     }
 
     public static ArrayList<String> starHelper(ArrayList<String> starArr,
                                                Integer N,
                                                Integer holeSize,
-                                               Integer row,
-                                               Integer start,
-                                               Integer end) {
-        if(N < 3){
+                                               Integer x,
+                                               Integer y,
+                                               Integer cnt) {
+        if(holeSize < 1){
             return starArr;
         }
-        String starRow = starArr.get(row);
+        String starRow = starArr.get(y);
         StringBuilder sb = new StringBuilder(starRow);
 
         // 지정된 위치의 문자 교체
         ArrayList<String> spacesArr = new ArrayList<>(Collections.nCopies(holeSize, " "));
         String spaceString = String.join("", spacesArr);
-        sb.replace( start, end, spaceString);
-        starArr.set(row,sb.toString());
+        sb.replace( x, x + holeSize, spaceString);
+        starArr.set(y,sb.toString());
 
-        Integer newRow = row + 1;
-        if(row < N*2/3){
-            starHelper(starArr, N, holeSize, newRow,start,end);
+        Integer newY = y + holeSize * 3;
+        Integer newX = x + holeSize * 3;
+
+        // 교체했으면
+        // 1.옆으로 가야할지
+        // 2.다음 줄로 가야할지
+        // 3.새로운 위치에서 시작해야할지
+        // 4. 다른 크기의 구멍으로 시작해야할지
+        // 5. 멈춰야 할지
+        for (String a : starArr){
+            System.out.println(a);
         }
+        System.out.println();
 
-        Integer newHoleSize = N/3;
-        Integer newStart = N/3;
-        Integer newEnd = 2 * N/3;
+        if(cnt > 1){
+            if(N < newX ){
+                //바로 아랫줄 고치기
+                starHelper(starArr, N, holeSize, holeSize,y + 1 ,cnt-1);
+            } else{
+                // 옆으로 이동하기
+                starHelper(starArr, N, holeSize, newX, y,cnt);
+            }
+        } else {
+            if(N < newY ){
+                starHelper(starArr, N, holeSize/3, holeSize/3, holeSize/3,holeSize/3);
+            } else{
+                if(N < newX ){
+                    starHelper(starArr, N, holeSize, holeSize, y,cnt);
+                }else{
+                    starHelper(starArr, N, holeSize, newX, y,cnt);
+                }
 
-        starHelper(starArr, N, newHoleSize, newRow,newStart,newEnd);
-        starHelper(starArr, N, newHoleSize, newRow,newStart,newEnd);
-        starHelper(starArr, N, newHoleSize, newRow,newStart,newEnd);
+            }
+        }
 
         return starArr;
     }
