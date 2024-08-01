c3745f4db0f8fde089e0475d0b397dda9dc2586d
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-08-01 13:20:17 +0900

    Solve15651 반영,  Solve15650 수정

--- java-algorithms/step-by-step/backTracking/Solve15651.java
+++ java-algorithms/step-by-step/backTracking/Solve15651.java
@@ -2,26 +2,23 @@ package backTracking;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
-public class Solve15650 {
-    // 문제 : N과 M (2)
+public class Solve15651 {
+    // 문제 : N과 M (3)
 
-    // 해결책 : 숫자 N만큼 for문을 M만큼 재귀한다. 재귀 완료시 저장한다. (조합을 구하자)
+    // 해결책 : 숫자 N만큼 for문을 M만큼 재귀한다.
     //
     // N : 숫자 범위
     // M : 수열 개수
     // solve : 재귀실행 함수
     // acc : 재귀할때마다 숫자가 담기는 배열
     // resultArr : acc가 담기는 배열
-    // start : 재귀할때 시작범위. 이전에 담긴 숫자만큼 늘어난다.
     //
     //  1. N과 M으로 solve의 스펙을 정의한다.
     //   1-1. N만큼 숫자를 탐색한다.
-    //   1-2. acc에 숫자가 없다면 추가한다.
-    //   1-3. N, acc, M - 1, acc에 담긴 수로 재귀한다. (이전 acc에 담긴 수 부터 N까지 탐색하며 반복한다)
+    //   1-2. N, acc, M - 1 로 재귀한다.
     //
     //  2. M이 0이되면, M번 재귀가 끝났으므로 acc배열을 resultArr에 저장한다.
     //  3. 리턴된 resultArr을 출력한다.
@@ -37,7 +34,7 @@ public class Solve15650 {
             Integer n = Integer.parseInt(st.nextToken());
             Integer m = Integer.parseInt(st.nextToken());
             // 재귀 수행
-            solve(mutableArr, new ArrayList<>(), n, m, 1);
+            solve(mutableArr, new ArrayList<>(), n, m);
         }
         try (OutputStreamWriter osw = new OutputStreamWriter(System.out); BufferedWriter bw = new BufferedWriter(osw)) {
             for (ArrayList<Integer> obj : mutableArr) {
@@ -53,19 +50,15 @@ public class Solve15650 {
     public static void solve(ArrayList<ArrayList<Integer>> resultArr,
                              ArrayList<Integer> acc,
                              Integer n,
-                             Integer m,
-                             Integer start) {
+                             Integer m) {
 
         if (m.equals(0)) {
             resultArr.add(new ArrayList<>(acc));
             return;
         }
-        for (int i = start; i <= n; i++) {
-            if (acc.contains(i)) {
-                continue;
-            }
+        for (int i = 1; i <= n; i++) {
             acc.add(i);
-            solve(resultArr, acc, n, m - 1, i);
+            solve(resultArr, acc, n, m - 1);
             acc.remove(acc.size() - 1); // 재귀 호출이 끝난 후, 리스트에서 마지막 추가된 요소 제거
         }
     }
