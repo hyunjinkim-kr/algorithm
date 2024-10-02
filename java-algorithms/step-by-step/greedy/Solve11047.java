f653be532d7226a6a830904f1669aaaaaa4e3ed7
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-10-02 18:47:49 +0900

    pr 템플릿 설정

--- java-algorithms/step-by-step/greedy/Solve11047.java
+++ java-algorithms/step-by-step/greedy/Solve11047.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Solve11047 {
-  // 문제 : 동전 0 dev test2233
+  // 문제 : 동전 0
 
   // 해결책 : 총 금액에서 가장큰 동전만큼 빼고, 금액보다 동전이 큰 경우 작은동전으로 반복한다.
   //
@@ -20,13 +20,13 @@ public class Solve11047 {
   //
   // 3. 금액K에서, 가장 큰 단위의 금액부터 빼며 0이 될때까지 반복한 후 횟수를 리턴한다.
   //  - 3-1. moneyArr의 마지막 값 (가장 큰 동전)을 가져온다.
-  //  - 3-2. K보다 작은경우, K에서 뺀다.
-  //  - 3-3. K보다 큰 경우, moneyArr에서 다음으로 큰 값을 가져온다.
+  //  - 3-2. K를 동전으로 나눈 값을 cnt에 저장한다.
+  //  - 3-3. K % 동전 (나머지) 값을 K로 설정한다.
   //  - 3-4. K가 0이 될때까지 반복한다.
   //
   // 4. cnt를 출력한다.
   //
-  // -- 시간복잡도 O(K)
+  // -- 시간복잡도 O(N)
   public static void main(String[] args) throws IOException {
     Integer result;
     try (InputStreamReader isr = new InputStreamReader(System.in);
@@ -53,12 +53,9 @@ public class Solve11047 {
     Integer idx = moneyArr.size() - 1;
     Integer cnt = 0;
     while (!k.equals(0)) {
-      if (moneyArr.get(idx) > k) {
-        idx--;
-      } else {
-        k -= moneyArr.get(idx);
-        cnt++;
-      }
+      cnt += k / moneyArr.get(idx);
+      k = k % moneyArr.get(idx);
+      idx--;
     }
     return cnt;
   }
