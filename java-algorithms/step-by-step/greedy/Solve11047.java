059b241f1586df3cce98d6277ca14d0ba041a749
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-10-02 19:03:36 +0900

    Solve11047 수정

--- java-algorithms/step-by-step/greedy/Solve11047.java
+++ java-algorithms/step-by-step/greedy/Solve11047.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Solve11047 {
-    // 문제 : 동전 0 dev test
+    // 문제 : 동전 0
 
     // 해결책 : 총 금액에서 가장큰 동전만큼 빼고, 금액보다 동전이 큰 경우 작은동전으로 반복한다.
     //
@@ -16,17 +16,17 @@ public class Solve11047 {
     // 1. N과 K을 입력받는다.
     //
     // 2. N번 입력받은 동전종류를 moneyArr 에 저장한다.
-    //      복잡도 N
+    //    복잡도 N
     //
     // 3. 금액K에서, 가장 큰 단위의 금액부터 빼며 0이 될때까지 반복한 후 횟수를 리턴한다.
     //  - 3-1. moneyArr의 마지막 값 (가장 큰 동전)을 가져온다.
-    //  - 3-2. K보다 작은경우, K에서 뺀다.
-    //  - 3-3. K보다 큰 경우, moneyArr에서 다음으로 큰 값을 가져온다.
+    //  - 3-2. K를 동전으로 나눈 값을 cnt에 저장한다.
+    //  - 3-3. K % 동전 (나머지) 값을 K로 설정한다.
     //  - 3-4. K가 0이 될때까지 반복한다.
     //
     // 4. cnt를 출력한다.
     //
-    // -- 시간복잡도 O(K)
+    // -- 시간복잡도 O(N)
     public static void main(String[] args) throws IOException {
         Integer result;
         try (InputStreamReader isr = new InputStreamReader(System.in);
@@ -36,28 +36,26 @@ public class Solve11047 {
             Integer k = Integer.parseInt(st.nextToken());
             ArrayList<Integer> moneyArr = new ArrayList<>();
 
-            for(int i = 0; i < n; i++){
+            for (int i = 0; i < n; i++) {
                 StringTokenizer st2 = new StringTokenizer(br.readLine());
                 Integer money = Integer.parseInt(st2.nextToken());
                 moneyArr.add(money);
             }
-            result = solve(moneyArr,k);
+            result = solve(moneyArr, k);
         }
-        try(OutputStreamWriter osw = new OutputStreamWriter(System.out);
-            BufferedWriter bw = new BufferedWriter(osw)){
+        try (OutputStreamWriter osw = new OutputStreamWriter(System.out);
+             BufferedWriter bw = new BufferedWriter(osw)) {
             bw.write(result + "");
         }
     }
-    public static Integer solve(ArrayList<Integer>moneyArr, Integer k){
+
+    public static Integer solve(ArrayList<Integer> moneyArr, Integer k) {
         Integer idx = moneyArr.size() - 1;
         Integer cnt = 0;
-        while (!k.equals(0)){
-            if(moneyArr.get(idx) > k){
-                idx--;
-            } else {
-                k -= moneyArr.get(idx);
-                cnt++;
-            }
+        while (!k.equals(0)) {
+            cnt += k / moneyArr.get(idx);
+            k = k % moneyArr.get(idx);
+            idx--;
         }
         return cnt;
     }
