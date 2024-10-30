1e9a4a1dc87465c40080e71cacaa14c584a9b94f
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-10-30 15:01:49 +0900

    1920 수 찾기 수정 : 튜플() 사용, position 배열 생성 및 리턴방법 변경

--- python-algorithm/step-by-step/divideConquer/solve_2630.py
+++ python-algorithm/step-by-step/divideConquer/solve_2630.py
@@ -9,16 +9,16 @@ import sys
 # - x : 행
 # - y : 열
 # - size : 탐색하는 종이의 범위
+# - positions : 4부분의 위치를 저장하는 배열
 # - white : 흰색 (0) 정사각형의 개수
 # - blue : 파란색 (1) 정사각형의 개수
 #
 ## 알고리즘 설명
 # 1. N 과 N 길이의 색종이 내용을 입력받아 array에 저장한다.
-# 2. 색종이 사이즈가 1인경우, x,y에 해당하는 array의 숫자가 0이면 [1,0]을, 1이면 [0,1]을 리턴한다. 사이즈가 1이 아닌경우, 아래 과정을 반복한다.
-#  2-1 사이즈를 2로 나눈후, 초기 크기의 4등분 크기의 사각형에 대해 재귀하며 계산한다.
-#  2-2 각 사각형의 결과가 모두 같은경우, 리턴된 배열의 0번째 값이 1이면 흰색, 1번째 값이 1이면 파란색을 리턴한다.
-#    e.g) 0일떄 return [1,0]
-#  2-3 모두 같지 않은경우 white에 모든 사각형의 0번째 값을 합하고, blue에 모든 사각형의 1번째 값을 합하여 리턴한다.
+# 2. 색종이 사이즈가 1인경우, x,y에 해당하는 array의 숫자가 0이면 (1,0)을, 1이면 (0,1)을 리턴한다. 사이즈가 1이 아닌경우, 아래 과정을 반복한다.
+#  2-1 사이즈를 2로 나눈후, 초기 크기의 4등분에 해당하는 positions의 사각형에 대해 재귀하며 계산한다.
+#  2-2 리턴된 blue 값이 0이면 흰색 (1,0), white 값이 0이면 파란색 (0,1)을 리턴한다.
+#  2-3 모두 같지 않은경우 (white,blue)를 리턴한다.
 # 3. white 와 blue를 출력한다.
 #
 #
@@ -45,37 +45,42 @@ def main():
 
 
 def solve(array, n):
-    return solve_inner(array, 0, 0, n)
+    return chk_paper_color(array, 0, 0, n)
 
 
 # 분할정복 함수
-def solve_inner(array, x, y, size):
+def chk_paper_color(array, x, y, size):
     # 현재 영역이 모두 같은 값인지 확인하는 함수
     if size == 1:
         if array[x][y] == 0:
-            return [1, 0]  # 하얀색 (0)
+            return (1, 0)  # 하얀색 (0)
         else:
-            return [0, 1]  # 파란색 (1)
+            return (0, 1)  # 파란색 (1)
     # 4등분으로 나누어 재귀 호출
     half_size = size // 2
-    result1 = solve_inner(array, x, y, half_size)  # 왼쪽 위
-    result2 = solve_inner(array, x, y + half_size, half_size)  # 오른쪽 위
-    result3 = solve_inner(array, x + half_size, y, half_size)  # 왼쪽 아래
-    result4 = solve_inner(array, x + half_size, y + half_size, half_size)  # 오른쪽 아래
-    # 네 부분에서 얻은 결과를 합산
-    white = result1[0] + result2[0] + result3[0] + result4[0]
-    blue = result1[1] + result2[1] + result3[1] + result4[1]
+    positions = [
+        (x, y),
+        (x, y + half_size),
+        (x + half_size, y),
+        (x + half_size, y + half_size),
+    ]
+
+    results = (0, 0)
+    for x, y in positions:
+        zipped = zip(results, chk_paper_color(array, x, y, half_size))
+        results = tuple(map(sum, zipped))
+
+    white = results[0]
+    blue = results[1]
 
     # 만약 4개의 구역이 모두 동일하다면, 그 구역을 하나로 합칠 수 있음
-    if result1 == result2 == result3 == result4:
-        # 만약 4개의 하위 구역이 모두 하얀색 혹은 파란색일 경우
-        if result1[0] == 1:  # 모두 하얀색(0)
-            return [1, 0]
-        elif result1[1] == 1:  # 모두 파란색(1)
-            return [0, 1]
+    if blue == 0:  # 모두 하얀색(0)
+        return (1, 0)
+    elif white == 0:  # 모두 파란색(1)
+        return (0, 1)
 
     # 그렇지 않으면 그대로 합산된 결과를 반환
-    return [white, blue]
+    return (white, blue)
 
 
 if __name__ == "__main__":
