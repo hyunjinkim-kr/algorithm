0b96004651fa04eea59bb7418ff32ed4b47f87df
hyunjinkim-kr <dufqnfsk22@gmail.com>
2024-11-05 18:39:55 +0900

    Solve11279 최대 힙 수정 : addOrPoll 파라미터 정의 (Command 클래스 type,value) +

--- java-algorithms/step-by-step/priorityQueue/Solve11279.java
+++ java-algorithms/step-by-step/priorityQueue/Solve11279.java
@@ -12,17 +12,17 @@ public class Solve11279 {
   // arr1 : 입력받은 N개 숫자가 저장되는 배열
   // solve : arr1의 값과 우선순위 큐 pq1에 대한 addOrPoll의 결과를 리턴하는 함수.
   // addOrPoll : 우선순위 큐에 arr1의 poll 혹은 add 를 하여 결과를 리턴하는 함수.
-  // pq1 : 0을 제외한 arr1의 숫자가 저장될 우선순위 큐. (단, add가 될때는 -1을 저장해둔다)
-  // pollNum : pq1에서 poll 한 값. 가장 큰 값에 해당한다.
+  // pq1 : 0을 제외한 arr1의 숫자가 저장될 우선순위 큐.
   // resultArr : arr1 입력에 따라 출력된 결과가 저장될 배열.
+  // Command : 명령의 타입과 숫자를 정의하는 클래스
   //
   //  1. N과 N번 만큼의 숫자를 입력받고, arr1에 저장한다.
   //  2. arr1을 파라미터로 하여 solve 함수를 수행한다.
   //   2-1. 내림차순 pq1을 생성한다.
   //   2-2. pq1 우선순위 큐와 arr1의 숫자를 순서대로 파라미터로하여 addOrPoll 함수를 수행한다. 리턴된 값은 resultArr에 저장된다.
   //     case1 0인경우 : pq1을 poll한 값을 리턴한다. 단, pq1이 비었을때는 0을 리턴한다.
-  //     case2 자연수인 경우 : pq1에 값을 저장한다. -1을 리턴한다.
-  //   2-3. -1 이외의 값을 resultArr에 저장하고 리턴한다.
+  //     case2 자연수인 경우 : pq1에 값을 저장한다. Optional.empty를 리턴한다.
+  //   2-3. 리턴된 값이 있으면, resultArr에 저장하고 리턴한다.
   //  3. 리턴된 resultArr을 출력한다.
   //
   //
@@ -53,24 +53,58 @@ public class Solve11279 {
 
     for (int i = 0; i < arr1.size(); i++) {
       Integer num = arr1.get(i);
-      Integer result = addOrPoll(pq1, num);
-      if (!result.equals(-1)) {
-        resultArr.add(result);
+      Optional<Integer> result;
+
+      if (num.equals(0)) {
+        // num이 0일 때는 POLL 명령 실행
+        result = addOrPoll(pq1, new Command(CommandType.POLL, null));
+      } else {
+        // num이 0이 아닐 때는 ADD 명령 실행
+        result = addOrPoll(pq1, new Command(CommandType.ADD, num));
       }
+      result.ifPresent(value -> resultArr.add(value));
     }
     return resultArr;
   }
 
-  public static Integer addOrPoll(PriorityQueue<Integer> pq1, Integer num) {
-    if (num.equals(0)) {
-      Integer pollNum = pq1.poll();
-      if (pollNum == null) {
-        pollNum = 0;
-      }
-      return pollNum;
-    } else {
-      pq1.add(num);
-      return -1;
+  public static Optional<Integer> addOrPoll(PriorityQueue<Integer> pq1, Command command) {
+
+    switch (command.getType()) {
+      case ADD:
+        if (command.getValue() != null) {
+          pq1.add(command.getValue());
+          return Optional.empty();
+        }
+        break;
+
+      case POLL:
+        Integer removed = pq1.poll();
+        return Optional.of((removed != null) ? removed : 0);
+    }
+    return Optional.empty();
+  }
+
+  enum CommandType {
+    ADD,
+    POLL
+  }
+
+  // Command 클래스 정의
+  static class Command {
+    private final CommandType type;
+    private final Integer value;
+
+    public Command(CommandType type, Integer value) {
+      this.type = type;
+      this.value = value;
+    }
+
+    public CommandType getType() {
+      return type;
+    }
+
+    public Integer getValue() {
+      return value;
     }
   }
 }
